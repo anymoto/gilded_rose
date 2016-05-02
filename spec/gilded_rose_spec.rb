375b353384cc66cc958e0a527af859670263e784
Ana Castro <ana@castromartinez.com>
2016-05-01 21:06:11 -0500

    Improve specs wording

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,51 +2,36 @@ require_relative '../lib/gilded_rose'
 
 describe GildedRose, "#update_quality" do
 
-  ######### Regular item contexts
+  ######### Basic Item contexts
 
-  context "with a single regular item" do
-    let(:initial_sell_in) { 5 }
-    let(:initial_quality) { 10 }
-    let(:name) { "item" }
-    let(:item) { Item.new(name, initial_sell_in, initial_quality) }
+  context 'with a single basic item' do
+    let(:item) { Item.new('item', 5, 10) }
     let(:subject) { GildedRose.new([item]) }
 
-    before { subject.update_quality }
-
     it 'decreases sell in date' do
-      expect(item.sell_in).to eq(4)
+      expect{ subject.update_quality }.to change{ item.sell_in }.from(5).to(4)
     end
 
     it 'decreases quality' do
-      expect(item.quality).to eq(9)
+      expect{ subject.update_quality }.to change{ item.quality }.from(10).to(9)
     end
   end
 
   context 'when the regular item sell by date has passed' do
-    let(:initial_sell_in) { 0 }
-    let(:initial_quality) { 10 }
-    let(:name) { "item" }
-    let(:item) { Item.new(name, initial_sell_in, initial_quality) }
+    let(:item) { Item.new('item', 0, 10) }
     let(:subject) { GildedRose.new([item]) }
 
-    before { subject.update_quality }
-
     it 'decreases quality twice as fast' do
-      expect(item.quality).to eq(8)
+      expect{ subject.update_quality }.to change{ item.quality }.from(10).to(8)
     end
   end
 
   context 'when the quality is zero and sell by date has passed' do
-    let(:initial_sell_in) { 0 }
-    let(:initial_quality) { 0 }
-    let(:name) { "item" }
-    let(:item) { Item.new(name, initial_sell_in, initial_quality) }
+    let(:item) { Item.new('item', 0, 0) }
     let(:subject) { GildedRose.new([item]) }
 
-    before { subject.update_quality }
-
-    it 'does not set a negative number to quality' do
-      expect(item.quality).to eq(0)
+    it 'does not decrease quality' do
+      expect{ subject.update_quality }.to_not change{ item.quality }
     end
   end
 
@@ -56,14 +41,12 @@ describe GildedRose, "#update_quality" do
     let(:item) { Item.new('Aged Brie', 3, 10) }
     let(:subject) { GildedRose.new([item]) }
 
-    before { subject.update_quality }
-
     it 'decreases sell in date' do
-      expect(item.sell_in).to eq(2)
+      expect{ subject.update_quality }.to change{ item.sell_in }.from(3).to(2)
     end
 
     it 'increases quality' do
-      expect(item.quality).to eq(11)
+      expect{ subject.update_quality }.to change{ item.quality }.by(1)
     end
   end
 
@@ -71,10 +54,8 @@ describe GildedRose, "#update_quality" do
     let(:item) { Item.new('Aged Brie', 3, 50) }
     let(:subject) { GildedRose.new([item]) }
 
-    before { subject.update_quality }
-
     it 'does not increases quality' do
-      expect(item.quality).to eq(50)
+      expect{ subject.update_quality }.to_not change{ item.quality }
     end
   end
 
@@ -93,9 +74,9 @@ describe GildedRose, "#update_quality" do
     end
   end
 
-  ######### Backstage Passes contexts
+  ######### Backstage Pass contexts
 
-  context 'with Backstage Passes with more than 10 days left' do
+  context 'with Backstage Pass with more than 10 days left' do
     let(:item) { Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 10) }
     let(:subject) { GildedRose.new([item]) }
 
@@ -104,7 +85,7 @@ describe GildedRose, "#update_quality" do
     end
   end
 
-  context 'with Backstage Passes with 10 days or less left' do
+  context 'with Backstage Pass with 10 days or less left' do
     let(:item) { Item.new('Backstage passes to a TAFKAL80ETC concert', 8, 10) }
     let(:subject) { GildedRose.new([item]) }
 
@@ -113,12 +94,21 @@ describe GildedRose, "#update_quality" do
     end
   end
 
-  context 'with Backstage Passes with 5 days or less left' do
+  context 'with Backstage Pass with 5 days or less left' do
     let(:item) { Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 10) }
     let(:subject) { GildedRose.new([item]) }
 
     it 'increases quality by 3' do
       expect{ subject.update_quality }.to change{ item.quality }.by(3)
+    end
+  end
+
+  context 'when Backstage Pass sell in date has passed' do
+    let(:item) { Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 10) }
+    let(:subject) { GildedRose.new([item]) }
+
+    it 'drops quality to 0' do
+      expect{ subject.update_quality}.to change{ item.quality }.from(10).to(0)
     end
   end
 
@@ -137,12 +127,12 @@ describe GildedRose, "#update_quality" do
     end
   end
 
-  context 'with Conjured quality equals to 1' do
-    let(:item) { Item.new('Conjured', 5, 1) }
+  context 'with Conjured quality equals to zero' do
+    let(:item) { Item.new('Conjured', 5, 0) }
     let(:subject) { GildedRose.new([item]) }
 
-    it 'set quality as zero' do
-      expect{ subject.update_quality }.to change{ item.quality }.from(1).to(0)
+    it 'does not decrease quality' do
+      expect{ subject.update_quality }.to_not change{ item.quality }
     end
   end
 
